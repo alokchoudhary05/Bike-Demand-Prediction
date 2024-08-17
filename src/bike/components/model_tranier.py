9a7845ad7ad889677101e59255e2b9120ceb1cfe
alokchoudhary05 <alokkumarchoudhary03@gmail.com>
2024-08-17 17:27:17 +0530

    MLFLOW

--- src/bike/components/model_tranier.py
+++ src/bike/components/model_tranier.py
@@ -122,7 +122,37 @@ class ModelTrainer:
 
             best_params = params[actual_model]
 
+            #ML flow
 
+
+            mlflow.set_registry_uri("https://dagshub.com/alokchoudhary05/Bike-Demand-Prediction.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
 
 
 
